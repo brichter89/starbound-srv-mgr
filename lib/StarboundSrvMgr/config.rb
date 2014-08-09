3e91198a3fb006678583f86ca8561eed4ae412ca
Björn Richter <bjoern.richter@mayflower.de>
2014-08-10 00:55:58 +0200

    Retrieve configuration from yaml file or hash

--- lib/StarboundSrvMgr/config.rb
+++ lib/StarboundSrvMgr/config.rb
@@ -1,12 +1,29 @@
+require 'yaml'
+
 module StarboundSrvMgr
     class Config
 
+        # @var Hash
+        @config
+
         # Initialize the config object with a configuration file or hash.
         #
-        # @param [String] configFile
+        # @param [String|Hash] config_file
         # @return [StarboundSrvMgr::Config]
-        def initialize(configFile)
+        def initialize(config_file)
+            @config = case config_file
+                          when String
+                              YAML.load_file config_file
+                          when Hash
+                              config_file
+                      end
+        end
 
+        # Get a config value by key
+        #
+        # @param [String|Symbol] key
+        def get(key)
+            @config[key.to_s]
         end
 
     end
--- spec/StarboundSrvMgr/config_spec.rb
+++ spec/StarboundSrvMgr/config_spec.rb
@@ -24,4 +24,12 @@ describe StarboundSrvMgr::Config do
 
         expect(config).to be_instance_of StarboundSrvMgr::Config
     end
+
+    it 'should return the value of a config key' do
+        expect(@config.get :bazinga).to eql 'BAZINGA'
+        expect(@config.get :Day_of_German_Unity).to eql Date.new(1990, 10, 3)
+        expect(@config.get :radius_of_earth_km).to eql 6371
+        expect(@config.get :enable_gravity).to be true
+        expect(@config.get :π).to eql Math::PI
+    end
 end
